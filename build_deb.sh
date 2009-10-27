1adaa6d578ea35b9bba560318e141d5bc9b1f867
public <public@kered.org>
2009-10-27 16:39:30 +0000

    versioning change

--- build_deb.sh
+++ build_deb.sh
@@ -1,10 +1,10 @@
 set -x
-VERSION="0.5.0"
+VERSION="0.5.1"
+VERSION="${VERSION}_r`svnversion .`"
 cd debs
 mkdir flyback
 cp ../src/* flyback
 cd flyback
-#VERSION="${VERSION}_r`svnversion .`"
 rm -Rf debs debian
 find .svn | xargs rm -Rf
 cd ..
